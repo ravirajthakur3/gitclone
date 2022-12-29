06374f429a919ff86fafff6e0be4226d0d96b770
ravirajthakur3 <rajthakur1997@outlook.com>
2022-12-30 00:53:29 +0530

    branchExample

--- exe.py
+++ exe.py
@@ -1,8 +1,5 @@
 print("we are indian")
 print("hello all")
-<<<<<<< HEAD
 a=23
-=======
->>>>>>> a584d746fd78762bdf4614ba973d382750bdd874
 str="hello"
->>>>>>> f986c0878d35a762b31b6383e2b2a89a2eeeafa1
+b=56
